327800b9a0429892f2c3107df9237c64300fa692
Oleg Schedrin <koboltkob@gmail.com>
2023-03-05 13:58:49 +0300

    Добавьте файлы проекта.

---
+++ Macro sub/Macro sub.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+#define sub(x,y) (x-(y))
+
+using namespace std;
+	
+int main()
+{
+	int a = 6;
+	int b = 5;
+	int c = 2;
+	cout << sub(a, b) << endl;
+	cout << sub(a, b) * c << endl;
+	cout << sub(a, b + c) * c << endl;
+}
+
