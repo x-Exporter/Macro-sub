87a726fc58eb5ed687ce45282b8518975784ff7a
Oleg Schedrin <koboltkob@gmail.com>
2023-03-06 14:52:23 +0300

    Change 1

--- Macro sub/Macro sub.cpp
+++ Macro sub/Macro sub.cpp
@@ -1,16 +1,15 @@
 ﻿#include <iostream>
 
-#define sub(x,y) (x-(y))
+#define sub(x,y) ((x)-(y))
 
 using namespace std;
-	
+
 int main()
 {
 	int a = 6;
 	int b = 5;
 	int c = 2;
-	cout << sub(a, b) << endl;
+	cout << sub(a = 5, b) << endl;
 	cout << sub(a, b) * c << endl;
 	cout << sub(a, b + c) * c << endl;
 }
-
